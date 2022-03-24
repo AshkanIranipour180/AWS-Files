b68aff321646209ab78fa1b675c358e924ff4bc7
AshkanIranipour180 <47744180+AshkanIranipour180@users.noreply.github.com>
2022-03-24 17:34:07 +0000

    Update AWSmove.py

--- AWSmove.py
+++ AWSmove.py
@@ -1,3 +1,6 @@
 import boto3
+
+s3_client = boto3.client('s3', region_name='us-east-1', aws_access_key_id=vockey)
+
 s3 = boto3.resource('s3')
 s3.meta.client.upload_file('image1.jpg', 'mybucketashkan123', 'image1.jpg')
