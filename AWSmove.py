f38a0499fca01e5e3c354b5c99352129a9b4f2c6
AshkanIranipour180 <47744180+AshkanIranipour180@users.noreply.github.com>
2022-03-29 10:33:06 +0100

    Update AWSmove.py

--- AWSmove.py
+++ AWSmove.py
@@ -1,3 +1,20 @@
 import boto3
+import time
 s3 = boto3.resource('s3')
 s3.meta.client.upload_file('image1.jpg', 'mybucketashkan123', 'image1.jpg')
+
+time.sleep(30) # Sleep for 30 seconds
+
+s3.meta.client.upload_file('image2.jpg', 'mybucketashkan123', 'image2.jpg')
+
+time.sleep(30) # Sleep for 30 seconds
+
+s3.meta.client.upload_file('image3.jpg', 'mybucketashkan123', 'image3.jpg')
+
+time.sleep(30) # Sleep for 30 seconds
+
+s3.meta.client.upload_file('image4.jpg', 'mybucketashkan123', 'image4.jpg')
+
+time.sleep(30) # Sleep for 30 seconds
+
+s3.meta.client.upload_file('image5.jpg', 'mybucketashkan123', 'image5.jpg')
